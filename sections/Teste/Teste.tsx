4318d663bb17d2e20341a24d95f30edc8313754f
Helio Moraes <heliohenriquemoraes@gmail.com>
2024-02-28 19:36:26 -0300

    aloha

--- sections/Teste/Teste.tsx
+++ sections/Teste/Teste.tsx
@@ -6,7 +6,7 @@ export default function Teste({ title }: Props) {
   return (
     <div>
       <h1 class="font-bold">{title}</h1>
-      <p>This is an example section</p>
+      <p>This is an examplaw section</p>
     </div>
   );
 }
